0a54356562075944f43f1defcb0d48534419b281
Samuel K. Grush <Samuel@Grush.org>
2017-12-11 19:06:29 -0600

    Modify receive() for cut-off messages

--- helper_helper.py
+++ helper_helper.py
@@ -4,12 +4,17 @@ import pickle
 def receive(sock):
     rawsize = sock.recv(128)
     size = int.from_bytes(rawsize, 'big')
-    r = sock.recv(size)
-    try:
-        return pickle.loads(r)
-    except Exception as err:
-        print("###\n size = {}\n r = {}\n###".format(size, r))
-        raise
+
+    r = buf_r = b''
+    while len(r) < size:
+        if buf_r: print("###Going for seconds...")
+        buf_r = sock.recv(size - len(r))
+        if not buf_r:
+            if len(r) < size: print("###Predicting pickle error")
+            break
+        r += buf_r
+
+    return pickle.loads(r)
 
 def send(sock, data):
     pdata = pickle.dumps(data)
